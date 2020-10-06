4afb91ee05ddd873f6937700a0da151436149ef0
Samarpan Harit <haritsamarpan7@gmail.com>
2020-10-06 16:03:58 +0530

    Added data validation logic as per requirements

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 let checkPincodeBtn = document.getElementById("check-pincode");
 let pincodeInputBox = document.getElementById("pincode");
 let addToCartBtn = document.getElementById("addToCartBtn");
+let qtyCnt = document.getElementById("qty-count");
+let qtyIncBtn = document.getElementById("qty-increment");
+let qtyDecBtn = document.getElementById("qty-decrement");
+let quantity = parseInt(qtyCnt.innerText);
 
 checkPincodeBtn.onclick = () => {
 	let pincode = pincodeInputBox.value;
-	if (pincode) console.log(pincode);
+	console.log(pincode);
+	if (pincode !== "560017" && pincode !== "560103") {
+		addToCartBtn.disabled = true;
+	} else addToCartBtn.disabled = false;
 };
 
-/*
+qtyIncBtn.onclick = (e) => {
+	qtyCnt.innerText = ++quantity;
+};
 
-    The web page should be mobile responsive.
-    Disable Add to Cart button if pin codes are other than 560017, 560103.
-    Handle increment and decrement of quantity buttons.
-    The pricing should increase on the basis of quantity button.
-*/
+qtyDecBtn.onclick = (e) => {
+	if (quantity > 1) qtyCnt.innerText = --quantity;
+};
